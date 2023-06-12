392ba0ab350017e70533b2f49be6480cc250ba14
wildan <202210056@student.ibik.ac.id>
2023-06-12 01:23:20 -0700

    commit kedua

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import ArticleCard from './articlecard';
 
-function Article() {
-  return <h1>Article Page</h1>;
-}
+
+const Article = () => {
+  return (
+    <Container className="article">
+      <Row>
+        <Col>
+          <Image className='img-fluid' src="https://img.freepik.com/free-vector/eating-variety-foods-concept-illustration_114360-11295.jpg?size=626&ext=jpg&ga=GA1.1.696200391.1683087027&semt=robertav1_2_sidr" fluid />
+        </Col>
+      </Row>
+      <h1 className="h1-primary fw-semibold caption">Diet Sehat,
+       <br/>Masa Depan cerah</h1>
+      <p className="text-center">Saat ini terdapat berbagai jenis diet yang menjanjikan penurunan berat badan secara cepat. Namun, tidak semua jenis diet yang ampuh menurunkan berat badan aman bagi kesehatan Anda.
+      </p>
+      <p className="text-center2">D'Care hadir untuk membantu dalam program diet sehat Anda</p>
+      <div class='tengah-primary mb-1'>
+      <h2>Rekomendasi Diet</h2>
+      </div>
+      <ArticleCard/>
+    </Container>
+    
+  );
+};
 
 export default Article;
