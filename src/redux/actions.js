5438460462154a8d77c57c8954d8fb356d0fdd08
Luis <ivan_luis030602@protonmail.com>
2023-06-13 16:03:04 +0800

    add recommend food function (API not integrated yet)

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,18 +1,50 @@
-export const calculateBMI = (height, weight) => {
-  const parsedHeight = parseFloat(height);
-  const parsedWeight = parseFloat(weight);
+import axios from 'axios';
 
-  if (isNaN(parsedHeight) || isNaN(parsedWeight)) {
+export const calculateBMI = async (height, weight) => {
+  if (height && weight) {
+    const heightInMeters = height / 100;
+    const bmiValue = weight / (heightInMeters * heightInMeters);
+    const roundedBMI = bmiValue.toFixed(2);
+
+    let bmiCategory = '';
+    if (roundedBMI < 18.5) {
+      bmiCategory = 'Underweight';
+    } else if (roundedBMI >= 18.5 && roundedBMI < 25) {
+      bmiCategory = 'Ideal';
+    } else {
+      bmiCategory = 'Overweight';
+    }
+
+    const categoryMap = {
+      Underweight: 'high',
+      Ideal: 'medium',
+      Overweight: 'low'
+    };
+
+    const calLevel = categoryMap[bmiCategory];
+
+    let foodRecommendation = '';
+    try {
+      const response = await axios.get('https://back-end-production-643c.up.railway.app/food/');
+      const data = response.data;
+
+      if (data && data[bmiCategory]) {
+        foodRecommendation = data[bmiCategory];
+      }
+    } catch (error) {
+      console.log('Error fetching food recommendation:', error);
+    }
+
     return {
-      type: 'CALCULATE_BMI',
-      payload: null,
+      bmi: roundedBMI,
+      category: bmiCategory,
+      recommendation: foodRecommendation
+    };
+  } else {
+    return {
+      bmi: '',
+      category: '',
+      recommendation: ''
     };
   }
-
-  const bmi = parsedWeight / (parsedHeight * parsedHeight);
-
-  return {
-    type: 'CALCULATE_BMI',
-    payload: bmi.toFixed(2),
-  };
 };
