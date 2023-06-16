cbbf3b5b1f885d4df6afd88d1942a464f5dcbbe8
Luis <ivan_luis030602@protonmail.com>
2023-06-16 11:29:15 +0800

    Connect Login/Register API and navbar styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown, DropdownButton, NavDropdown } from 'react-bootstrap';
 
-function navBar() {
+function navBar({isLoggedIn, handleLogout}) {
+
+  // const [dropDown, setDropDown] = useState(false);
+  // const toggleDropdown = () => {
+  //   setDropDown(!dropDown);
+  // };
+
   return (
     <Navbar className='justify-content-between px-5 mainNavbar'>
       <div>
@@ -20,10 +26,17 @@ function navBar() {
         </Nav>
       </div>
       <div>
-        <Nav className='justify-content-center'>
+        {isLoggedIn && (
+          <NavDropdown title="Profile" align="end" className='dropdown'>
+            <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+          </NavDropdown>
+        )}
+        {!isLoggedIn && (
+          <Nav className='justify-content-center'>
           <Nav.Link><Link className='navlink' to="/login"><Button className='btn btn-outline-light login-btn'>Login</Button></Link></Nav.Link>
           <Nav.Link><Link className='navlink' to="/register"><Button className='btn btn-light join-btn'>Join</Button></Link></Nav.Link>
         </Nav>
+        )}
       </div>
     </Navbar>
   );
